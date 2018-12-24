0fbb2dbfb3577bc71778e5c7bb934a243fd18a01
Gabriel Martínez <gmartinez.code@gmail.com>
2018-12-23 18:56:33 -0600

    Create Example2.js

---
+++ Example2.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var url = "www.myportal.com.mx"
+const IP = "192.168.1.110";
+var value1 = 7 + 12;
+
+console.log(url, IP);
+alert("El resultado es:"+value);
+
+var number= 44;
+var caracter='Hola "que" pasa men';
+var boleano = false;
+
+
+
+var numero_falso = "34.65";
+console.log (typeof (Number(numero_falso)+7));
+console.log (typeof (parseInt(numero_falso)+7));
+console.log (typeof (parseFloat(numero_falso)+7));
+console.log (typeof (String(value1)+7));
+
