94edc32144e927a743afa983fc9890fceead6e61
Gabriel Martínez <gmartinez.code@gmail.com>
2018-12-23 18:35:24 -0600

    Create accordion.js

---
+++ accordion.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var age = 18;
+var name="Gabriel";
+/*
+operadores relacionales
+  >,<,>=,<=,==
+*/
+
+if(age != 18){
+  console.log(nombre + " tiene " + edad + "años")
+} else {
+  console.log(nombre + " tiene " + edad + "años")
+}
