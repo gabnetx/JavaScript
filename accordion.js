49d42b66abba6a66528b2fb324975bfe48678e46
Gabriel Martínez <gmartinez.code@gmail.com>
2018-12-23 19:06:03 -0600

    Update accordion.js

--- accordion.js
+++ accordion.js
@@ -2,9 +2,15 @@
 
 var age = 18;
 var name="Gabriel";
+var imprime="";
 /*
 operadores relacionales
   >,<,>=,<=,==
+ Operadores logicos
+  AND: &&
+  OR : ||
+  NEGACION: !
+  
 */
 
 if(age >= 18){
@@ -15,5 +21,26 @@ if(age >= 18){
    else{ console.log(nombre + "Ya no eres millenial"); }
   
 } else {
-  console.log(nombre + " tiene " + edad + "años, es menor de edad")
+  console.log(nombre + " tiene " + edad + "años, es menor de edad");
 }
+
+switch(age){
+  case 18:
+    imprime="Mayoria de edad";
+    break;
+  case 25:
+    imprime="Adulto";
+    break;
+  case 40:
+    imprime="Andropausia";
+    break;
+  default: 
+    imprime = "Neutral";
+}
+  
+  console.log(imprime);
+  
+var year=2018;
+if(year != 2017){ console.log ("El año no es 2017"); 
+if(year >= 2000 && year <= 2020){ console.log ("Estamos en la era actual");}
+  else {  console.log ("Estamos en la era post moderna"); }
